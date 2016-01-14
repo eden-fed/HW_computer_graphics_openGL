ad19f40803eedeec72aa9efdb700b6c828d8dcbb
Ben <ben123678@gmail.com>
2016-01-14 19:16:44 +0200

    fixed vertexShaderGouraudTM

--- Shaders/c_vertexShaderGouraudTM.glsl
+++ Shaders/c_vertexShaderGouraudTM.glsl
@@ -7,6 +7,7 @@ vec4 direction;
 vec4 intensity;
 };
 
+
 struct stMaterialProperties {
 float ambient;
 float diffuse;
@@ -33,16 +34,18 @@ void main()
 {
 vec4 color;
 vec4 P;
-vec4 N;
+
 vec4 L[2];
 vec4 R[2];
+vec4 N=vec4(0.0,0.0,0.0,1.0);
+vec4 eyePosition=vec4(0.0,0.0,0.0,1.0);
 
-N = normalize(NormalMatrix * VertexNormal);
+N.xyz = normalize(vec3(NormalMatrix * VertexNormal));
 P = MVMatrix * VertexPosition;
 
 for (int lNum = 0; lNum < 2; ++lNum) {
-		L[lNum]=vec4(0.0)
-		R[lNum]=vec4(0.0)
+		L[lNum]=vec4(0.0,0.0,0.0,1.0);
+		R[lNum]=vec4(0.0,0.0,0.0,1.0);
 		if (Lights[lNum].isEnabled){
 			if (Lights[lNum].isDirectional)
 				L[lNum] = Lights[lNum].position - Lights[lNum].direction;
@@ -56,20 +59,22 @@ for (int lNum = 0; lNum < 2; ++lNum) {
 
 gl_Position = MVPMatrix * VertexPosition;
 
-	L[1] = normalize(L[1]);
-	L[2] = normalize(L[2]);
-	R[1] = normalize(R[1]);
-	R[2] = normalize(R[2]);
+	L[0].xyz = normalize(vec3(L[0]));
+	L[1].xyz = normalize(vec3(L[1]));
+	R[0].xyz = normalize(vec3(R[0]));
+	R[1].xyz = normalize(vec3(R[1]));
 
 	//calculate ambiant lighting
 	vec4 ambient = material.ambient * AmbientProduct;
 
 	//init vectors
-	vec4 diffuse  = vec4(0.0,0.0,0.0);
-	vec4 specular = vec4(0.0,0.0,0.0) ;
+	vec4 diffuse  = vec4(0.0,0.0,0.0,1.0);
+	vec4 specular = vec4(0.0,0.0,0.0,1.0) ;
 	
 
-	vec4 V = normalize(eyePosition - P);					
+
+	vec4 V=vec4(0.0,0.0,0.0,1.0);
+	V.xyz = normalize(vec3(eyePosition - P));					
 	for (int lNum = 0; lNum < 2; ++lNum) {
 		if (Lights[lNum].isEnabled){
 					//calculate diffuse lighting
